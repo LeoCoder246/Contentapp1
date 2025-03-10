3482a2bf12e399af6253364b604bf4e02c678378
LeoCoder246 <shaikhabu246@gmail.com>
2025-03-10 15:03:09 +0530

    changes reverted

--- app.js
+++ app.js
@@ -20,4 +20,4 @@ app.use('/',staticRouter);
 app.use('/content', webcontentRouter)
  
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
-require('dotenv').config(); 
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = 'mongodb+srv://root:root@leocoder.a9thl.mongodb.net/blackdb?retryWrites=true&w=majority&appName=LeoCoder';
 
 const connectDB = async () => {
   try {mongoose.connect(MONGO_URI)
