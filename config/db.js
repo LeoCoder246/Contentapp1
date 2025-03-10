ed424082fbeb92666984ae10a73918e5a295a1c9
LeoCoder246 <shaikhabu246@gmail.com>
2025-03-10 14:52:41 +0530

    updated db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://root:root@leocoder.a9thl.mongodb.net/blackdb?retryWrites=true&w=majority&appName=LeoCoder';
+const MONGO_URI = process.env.MONGO_URI;
 
 const connectDB = async () => {
   try {mongoose.connect(MONGO_URI)
